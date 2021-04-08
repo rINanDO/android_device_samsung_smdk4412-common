d0594407ea2daa3c6aca7519aab04c1246c4f618
Dominggoes Isakh <drjisakh@gmail.com>
2021-04-08 20:55:55 +0200

    smdk4412-common: Binderize HWComposer

--- common.mk
+++ common.mk
@@ -110,6 +110,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl-exynos4 \
     android.hardware.graphics.mapper@2.0-impl-exynos4 \
     android.hardware.graphics.composer@2.1-impl \
+    android.hardware.graphics.composer@2.1-service \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     audio.a2dp.default \
