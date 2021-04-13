09abfd257d28b3ce76e72160fc73f6aa62826288
Dominggoes Isakh <drjisakh@gmail.com>
2021-04-13 20:42:32 +0200

    smdk4412-common: power: Use Exynos 4412 PowerHAL

--- common.mk
+++ common.mk
@@ -101,7 +101,7 @@ PRODUCT_PACKAGES += \
 
 # SamsungPowerHAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service.exynos4210
+    android.hardware.power@1.0-service.exynos4412
 
 # Battery
 PRODUCT_PACKAGES += \
